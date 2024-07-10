8d01ff6d603dd0d8f9709025b58f993a33041cc4
Naushad1234 <naushadali4741@gmail.com>
2024-07-10 21:08:21 +0530

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
+const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
