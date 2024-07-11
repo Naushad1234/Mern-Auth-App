87a052c052251d23ba52a119de74b9c28d9f3895
Naushad1234 <naushadali4741@gmail.com>
2024-07-11 22:11:21 +0530

    Created Test Api

--- api/index.js
+++ api/index.js
@@ -1,19 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js"; // ensure the file extension is included
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log("connected to MongoDB for real");
+    console.log("Connected to MongoDB for real");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Failed to connect to MongoDB", err);
   });
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("server listening on port 3000");
+  console.log("Server listening on port 3000");
 });
+
+// API Routes
+app.use("/api/user", userRoutes);
